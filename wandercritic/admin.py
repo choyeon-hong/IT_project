c79efbe7f259527a60f281bdf3fa89e123cb8b77
choyeon-hong <79977896+choyeon-hong@users.noreply.github.com>
2025-02-28 21:19:03 +0000

    remove DB files and  resolve conflicts

--- wandercritic/admin.py
+++ wandercritic/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from wandercritic.models import Place, Tour
+# from wandercritic.models import Place, Tour
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 # from wandercritic.models import UserProfile
 
-admin.site.register(Place)
-admin.site.register(Tour)
+# admin.site.register(Place)
+# admin.site.register(Tour)
 
 
 # Register your models here.
--- wandercritic/models.py
+++ wandercritic/models.py
@@ -3,47 +3,47 @@ from django.template.defaultfilters import slugify
 # from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
-class Place(models.Model):
-	pid = models.AutoField(primary_key=True)
-	pname = models.CharField(max_length=50, unique=True)
-	about = models.CharField(max_length=300)
-	picture = models.ImageField()
-	address = models.CharField(max_length=300)
-	geolocation = models.CharField(max_length=30, default='default_value') 
-	seasons = models.CharField(max_length=30)
-	activity = models.CharField(max_length=30)
-	travel_theme = models.CharField(max_length=30)
-	budget = models.IntegerField(default=0)
-	description = models.CharField(max_length=300)
+# class Place(models.Model):
+# 	pid = models.AutoField(primary_key=True)
+# 	pname = models.CharField(max_length=50, unique=True)
+# 	about = models.CharField(max_length=300)
+# 	picture = models.ImageField()
+# 	address = models.CharField(max_length=300)
+# 	geolocation = models.CharField(max_length=30, default='default_value') 
+# 	seasons = models.CharField(max_length=30)
+# 	activity = models.CharField(max_length=30)
+# 	travel_theme = models.CharField(max_length=30)
+# 	budget = models.IntegerField(default=0)
+# 	description = models.CharField(max_length=300)
 	
-	def __str__(self):
-		return self.pname
+# 	def __str__(self):
+# 		return self.pname
 
-class Tour(models.Model):
-	tname = models.CharField(max_length=50, unique=True)
-	place = models.ForeignKey(Place, on_delete=models.CASCADE)
-	information = models.CharField(max_length=128)
-	picture = models.ImageField()
-	address = models.CharField(max_length=300)
-	price = models.IntegerField(default=0)
+# class Tour(models.Model):
+# 	tname = models.CharField(max_length=50, unique=True)
+# 	place = models.ForeignKey(Place, on_delete=models.CASCADE)
+# 	information = models.CharField(max_length=128)
+# 	picture = models.ImageField()
+# 	address = models.CharField(max_length=300)
+# 	price = models.IntegerField(default=0)
 	
-	def __str__(self):
-		return self.tname
+# 	def __str__(self):
+# 		return self.tname
 	
-class Review(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	pid = models.ForeignKey(Place, on_delete=models.CASCADE)
-	rate = models.IntegerField()
-	description = models.CharField(max_length=300)
+# class Review(models.Model):
+# 	user = models.ForeignKey(User, on_delete=models.CASCADE)
+# 	pid = models.ForeignKey(Place, on_delete=models.CASCADE)
+# 	rate = models.IntegerField()
+# 	description = models.CharField(max_length=300)
 
-	def __str__(self):
-		return f"Review by {self.user.username}"
+# 	def __str__(self):
+# 		return f"Review by {self.user.username}"
 	
-class Admin(models.Model): 
-	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+# class Admin(models.Model): 
+# 	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
-	def __str__(self):
-		return self.user.username 
+# 	def __str__(self):
+# 		return self.user.username 
 
 class User(AbstractUser):
 	# pass
@@ -51,29 +51,30 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.username
-class UserProfile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-	profile = models.ImageField(upload_to='profile_pics')
+    
+# class UserProfile(models.Model):
+# 	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+# 	profile = models.ImageField(upload_to='profile_pics')
 
-	def __str__(self):
-		return self.user.username
+# 	def __str__(self):
+# 		return self.user.username
 
-class TravelAgent(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+# class TravelAgent(models.Model):
+# 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.user.username 
+# 	def __str__(self):
+# 		return self.user.username 
 
 
-class AdminPlaceManage(models.Model):
-	admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
-	place = models.ForeignKey(Place, on_delete=models.CASCADE)
+# class AdminPlaceManage(models.Model):
+# 	admin = models.ForeignKey(Admin, on_delete=models.CASCADE)
+# 	place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
-class TravelAgentTourManage(models.Model):
-	ta = models.ForeignKey(TravelAgent, on_delete=models.CASCADE)
-	tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+# class TravelAgentTourManage(models.Model):
+# 	ta = models.ForeignKey(TravelAgent, on_delete=models.CASCADE)
+# 	tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
 
-class UserPlaceView(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	place = models.ForeignKey(Place, on_delete=models.CASCADE)
+# class UserPlaceView(models.Model):
+# 	user = models.ForeignKey(User, on_delete=models.CASCADE)
+# 	place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
